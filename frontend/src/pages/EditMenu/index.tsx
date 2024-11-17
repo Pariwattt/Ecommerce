e1a57b579c39c7e5b832b5050eb9775683566f28
ZerKg <65112019@dpu.ac.th>
2024-11-18 01:52:33 +0700

    test

--- frontend/src/pages/EditMenu/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import EditMenu from '../../features/editMenu/edit';  // เรียกหน้า Menu มาเป็นหน้าแรก (Home Page)
-
-export default function EditPage() {
-  return <EditMenu />;
-}
--- frontend/src/pages/Summery3/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Summary from '../../features/summery/summary3';  // เรียกหน้า Menu มาเป็นหน้าแรก (Home Page)
-
-export default function Summary3Page() {
-  return <Summary />;
-}
