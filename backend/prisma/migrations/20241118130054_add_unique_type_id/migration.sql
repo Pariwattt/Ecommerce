7c5bfd03b81ba52cd4864fce515c25d6d193acbf
PhonchaiPhopila <65112127@dpu.ac.th>
2024-11-21 01:08:02 +0700

    Update User and Admin

--- backend/prisma/migrations/20241118130054_add_unique_type_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[typeID]` on the table `Type` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Type_typeID_key" ON "Type"("typeID");
--- frontend/src/pages/Payment/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Payment from '../../features/payment/payment';  // เรียกหน้า Menu มาเป็นหน้าแรก (Home Page)
-
-export default function HomePage() {
-  return <Payment />;
-}
