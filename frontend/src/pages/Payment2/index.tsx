7237a30b511f7932ebc6b410af45a9fe05604ae3
Rtnpb <65111521@dpu.ac.th>
2024-11-23 17:19:32 +0700

    ss

--- frontend/src/pages/Payment2/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Payment2 from '../../features/payment/payment2';  // เรียกหน้า Menu มาเป็นหน้าแรก (Home Page)
-
-export default function Payment2Page() {
-  return <Payment2 />;
-}
--- frontend/src/pages/Summery4/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Summary from '../../features/summery/summary4';  // เรียกหน้า Menu มาเป็นหน้าแรก (Home Page)
-
-export default function Summary4Page() {
-  return <Summary />;
-}
